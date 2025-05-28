806945943b1588363f6a6a4a04df72bc794c367e
rgdane <regadanew1@gmail.com>
2025-05-28 13:13:51 +0700

    department resources

---
+++ app/Filament/Resources/DepartmentResource.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\DepartmentResource\Pages;
+use App\Filament\Resources\DepartmentResource\RelationManagers;
+use App\Models\Department;
+use Filament\Forms;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class DepartmentResource extends Resource
+{
+    protected static ?string $model = Department::class;
+
+    protected static ?string $navigationIcon = 'hugeicons-departement';
+
+    protected static ?string $navigationLabel = 'Departemen';
+
+    protected static ?string $pluralLabel = 'Kelola Departemen';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('code')
+                    ->label('Kode')
+                    ->required()
+                    ->placeholder('Masukkan kode departemen')
+                    ->unique(ignoreRecord: true),
+
+                TextInput::make('name')
+                    ->label('Nama Departemen')
+                    ->placeholder('Masukkan nama departemen')
+                    ->required(),
+
+                Textarea::make('description')
+                    ->label('Deskripsi')
+                    ->placeholder('Masukkan deskripsi departemen')
+                    ->rows(3)
+                    ->columnSpan('full'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('code')
+                    ->label('Kode')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('name')
+                    ->label('Nama')
+                    ->searchable()
+                    ->sortable(),
+                
+                TextColumn::make('description')
+                    ->label('Deskripsi')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('users_count')
+                    ->label('Jumlah Pengguna')
+                    ->counts('users')
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListDepartments::route('/'),
+            'create' => Pages\CreateDepartment::route('/create'),
+            'edit' => Pages\EditDepartment::route('/{record}/edit'),
+        ];
+    }
+}
